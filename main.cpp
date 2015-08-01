7918108e0fee4ccafa2d0a8b6f81f8592cfc4032
linshan <boylinshan@gmail.com>
2015-08-01 22:30:39 +0800

    second commit

--- main.cpp
+++ main.cpp
@@ -5,42 +5,58 @@
 #include <mutex>
 #include <condition_variable>
 #include <unistd.h>
+#include <chrono>
 
 using namespace std;
 
 mutex rmtx,wmtx;
 condition_variable rcond,wcond;
 unique_lock<mutex> rlock,wlock;
+bool finished = false;
 string head="zhidao.baidu.com";
 
 void download(Downloader *dl, Spider *sp)
 {
     
     while(!sp->empty() || !dl->empty()){
-	sleep(1);
-	while(sp->empty())
-	    rcond.wait(rlock);
 
+	while(sp->empty()){
+	    rcond.wait_for(rlock,chrono::seconds(5));
+	    if(finished)return;
+	}
 	string url = head + sp->getUrl();
 
 	dl->setUrl(url);
+	cout << "downlowd" << endl;
 	dl->downloadPage();
+	cout << "finish" << endl;
 	wcond.notify_all();
+
     }
+    cout << "downloader exit" << endl;
+    finished = true;
+    
 }
 
 void spider(Spider *sp, Downloader *dl)
 {
     while(!sp->empty() || !dl->empty()){
-	while(dl->empty())
-    	    wcond.wait(rlock);
+
+	while(dl->empty()){
+	    wcond.wait_for(wlock,chrono::seconds(5));
+	    if(finished)return;
+	}
+
 
     	sp->loadPage(dl->getContext());
-	cout << "in" << endl;
+	cout << "search" << endl;
     	sp->search_for_link("");
 	cout << "out" << endl;
     	rcond.notify_all();
     }
+    cout << "spider exit" << endl;
+    finished = true;
+    
 }
 
 int 
